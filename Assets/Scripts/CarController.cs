eb014155bfe1f969750d9dcb2180625f8ba08957
Michiel Bakker <michielbakker.1995@gmail.com>
2020-04-18 13:25:20 +0200

    tweak car controls

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -24,15 +24,21 @@ public class CarController : MonoBehaviour
 
         float speed = Vector3.Dot(transform.forward, rb.velocity);
 
+        bool isControlling = Mathf.Abs(acceleration) > .1f || Mathf.Abs(steering) > .1f;
+        // bool isBreaking = !isControlling || (Mathf.Abs(acceleration) > .1f && ((acceleration > 0f) != (speed > 0f)));
         bool isBreaking = Mathf.Abs(acceleration) > .1f && ((acceleration > 0f) != (speed > 0f));
-        // bool isBreaking = Mathf.Abs(acceleration) > .1f && Mathf.Abs(speed) > .5f && ((acceleration > 0f) != (speed > 0f));
 
         float absSpeed = Mathf.Abs(speed);
 
+        float limitForce = 0f;
+        
         if (absSpeed > TargetSpeed) {
-            float limitForce = (absSpeed - TargetSpeed) * 1000f;
-            rb.AddForce(rb.velocity.normalized * -limitForce);
+            limitForce = (absSpeed - TargetSpeed) * 1000f;
+        } else if (!isControlling) {
+            limitForce = 1500f;
         }
+
+        rb.AddForce(rb.velocity.normalized * -limitForce);
 
         float brakeTorque = isBreaking ? BrakeTorque : 0f;
         float motorTorque = isBreaking ? 0f : acceleration * Torque;
@@ -48,5 +54,13 @@ public class CarController : MonoBehaviour
 
         FrontLeft.steerAngle = steering * SteerAngle;
         FrontRight.steerAngle = steering * SteerAngle;
+        
+        // self-righting
+        var angle = Vector3.Angle(transform.up, Vector3.up);
+        if(angle > 0.001)
+        {
+            var axis = Vector3.Cross(transform.up, Vector3.up);
+            rb.AddTorque(axis * (angle * 1000f));
+        }
     }
 }
